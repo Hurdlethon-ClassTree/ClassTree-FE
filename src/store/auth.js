d63dea8e3528ec09f4e56dba4e4af2f2b5b08f23
luna156 <63947262+luna156@users.noreply.github.com>
2024-11-22 22:39:53 +0900

    [Feat] logout 추가

--- src/store/auth.js
+++ src/store/auth.js
@@ -9,6 +9,9 @@ export default {
   mutations: {
     setToken(state, token) {
       state.token = token;
+    },
+    setNickname(state, nickname){
+      state.nickname = nickname;
     }
   },
   actions: {
@@ -18,6 +21,7 @@ export default {
 
         if (response.status === 200) {
           context.commit('setToken', response.data.token);
+          context.commit('setNickname', response.data.nickname);
         }
       } catch (e) {
         alert('아이디 혹은 비밀번호를 확인해주세요.');
@@ -27,10 +31,15 @@ export default {
       authApi.login(userId, password).then(response => {
         if (response.status === 200) {
           context.commit('setToken', response.data.token);
+          context.commit('setNickname', response.data.nickname);
         }
       }).catch(e => {
         alert('아이디 혹은 비밀번호를 확인해주세요.');
       });
+    },
+    logout(context){
+      context.commit('setToken', null);
+      context.commit('setNickname', null);
     }
   }
 }
